77ab0d646945ff27d2b5e01c484d9416b82f741a
Vipin Pundir <vipinpundir29@gmail.com>
2025-05-28 21:36:26 +0530

    fix all build error

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import { BackgroundBeams } from "@/components/ui/background-beams";
 import FormModal from "@/components/FormModel";
 import TypewriterInput from "@/components/TypewriterInput";
 
