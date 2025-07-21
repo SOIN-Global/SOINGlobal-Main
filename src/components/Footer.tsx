163885aeeb892853391c4637dfaa1f3db3c5cb96
Sukrit Saha <sukritsaha27@gmail.com>
2025-07-21 23:13:06 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,8 @@
+import Link from "next/link";
 import Image from "next/image";
 import React from "react";
 import logo from "@/assets/logos/soin-logo.png";
-import Link from "next/link";
+
 import {
   FaLinkedinIn,
   FaMedium,
