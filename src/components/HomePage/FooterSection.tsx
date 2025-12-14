b4db6a0f97308fff29a5bcaa3eda8551c89cab9d
peshkash17 <kalpeshpawar17680@gmail.com>
2025-12-14 22:40:29 +0530

    tele link updated

--- src/components/HomePage/FooterSection.tsx
+++ src/components/HomePage/FooterSection.tsx
@@ -85,10 +85,10 @@ export default function FooterSection() {
                             <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
                         </svg>
                     </Link>
-                    {/* <Link href="https://t.me/soin" target="_blank" rel="noopener noreferrer" className='text-white hover:text-white transition-colors'>
+                    <Link href="https://t.me/SOINGLOBALAnnoucement" target="_blank" rel="noopener noreferrer" className='text-white hover:text-white transition-colors'>
                         <Send className='w-5 h-5 md:w-6 md:h-6' />
                     </Link>
-                    <Link href="mailto:contact@soin.com" className='text-white hover:text-white transition-colors'>
+                    {/* <Link href="mailto:contact@soin.com" className='text-white hover:text-white transition-colors'>
                         <Mail className='w-5 h-5 md:w-6 md:h-6' />
                     </Link> */}
                 </div>
